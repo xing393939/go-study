789ea64905e717afdb9708b6fc89a83a36b87547
xing393939 <xing393939@users.noreply.github.com>
2022-01-25 10:13:03 +0800

    go-clean-architecture

---
+++ go-clean-architecture/app/db.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+)
+
+func InitDB() *gorm.DB {
+	//dsn := "root:123456@tcp(127.0.0.1:3306)?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "../sqlite.db"
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
