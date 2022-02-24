b1b8950453063938aa43b734a47c5d93b49c5c1e
xing393939 <xing393939@users.noreply.github.com>
2022-02-24 17:34:54 +0800

    go study

---
+++ go-demo/panic/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	hash := make([]int, 9, 98)
+	hash[0] = 1
+	hash[1] = 2
+	hash[2] = 3
+	say(hash)
+}
+
+//go:noinline
+func say(hash2 []int) {
+	hash2[3] = 4
+	_ = hash2
+}
