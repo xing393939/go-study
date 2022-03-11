5bd362dbe254ecc7d51654ffc4ffd4cd56e6c660
chenxing <chenxing@bookan.com.cn>
2022-03-11 17:01:46 +0800

    go study

--- docs/source-code-first-goroutine.md
+++ docs/source-code-first-goroutine.md
@@ -1,21 +1,13 @@
 ### 启动第一个协程
 
 <link rel="stylesheet" type="text/css" href="../images/jquery.dialog.css">
-<script>
-window.onload = function() {
-$.getScript("../images/jquery.dialog.js", function() {
-    $(".dialog1").dialog({
-        'title':'警告'
-    },function(api){
-        $('#btn1').click(function(){
-            api.open();
-        });
-    });
-});
-}
-</script>
+<script type=text/javascript src="../images/jquery.main.js"></script>
 
-<button id="btn1" class="button">打开对话框</button>
-<div class="dialog dialog1">你确定我够帅吗？</div>
+#### runtime.newproc
+
+<div id="newproc"></div>
+<script>
+    mainFunctions.push(['newproc', '#aaa'])
+</script>
 
 
---
+++ images/jquery.main.js
@@ -0,0 +1,31 @@
+let mainFunctions = [];
+window.onload = function () {
+    $.getScript("../images/jquery.dialog.js", function () {
+        mainFunctions.forEach(function (row) {
+            getSourceCode(row[0], row[1])
+        })
+    });
+};
+
+function getSourceCode(word, parent) {
+    let keywords = ['newproc1'];
+    $.get(`https://xing393939.github.io/static/go1.16.10/${word}.html`, function (text) {
+        let newElem = $(text);
+        let spans = newElem.find('span');
+        spans.each(function (k, span) {
+            if (keywords.includes(span.innerText) && word != span.innerText) {
+                spans.eq(k).html(`<a href="javascript:getSourceCode('${span.innerText}');">${span.innerText}</a>`)
+            }
+        });
+        if (parent) {
+            $(parent).append(newElem)
+        } else {
+            $('body').append(newElem);
+            newElem.dialog({
+                'title': word
+            }, function (api) {
+                api.open();
+            });
+        }
+    })
+}
