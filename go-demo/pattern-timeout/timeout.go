369dcb574dcb7f806c11ba265b03f078d155ed24
xing393939 <xing393939@users.noreply.github.com>
2022-04-08 20:44:26 +0800

    go study

--- go-demo/pattern-timeout/timeout.go
+++ go-demo/pattern-timeout/timeout.go
@@ -2,12 +2,30 @@ package main
 
 import "time"
 
+func httpRequestWithTimeout(url string, timeout time.Duration) string {
+	ch := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		ch <- "get from url: " + url
+	}()
+
+	select {
+	case result := <-ch:
+		return result
+	case <-time.After(timeout):
+		return "timeout"
+	}
+}
+
 func lessThan1Second(ch chan<- int) {
 	time.Sleep(time.Second)
 	ch <- 1
 }
 
 func main() {
+	println(httpRequestWithTimeout("1", time.Second))
+	println(httpRequestWithTimeout("1", time.Second*3))
+
 	ch := make(chan int, 1)
 	go lessThan1Second(ch)
 
