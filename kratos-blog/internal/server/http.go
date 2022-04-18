f91b615483893d14c524a3222a610bd8add9c10f
chenxing <chenxing@bookan.com.cn>
2022-04-18 10:47:21 +0800

    go study

---
+++ kratos-blog/internal/server/http.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/swagger-api/openapiv2"
+	v1 "go-study/kratos-blog/api/blog/v1"
+	"go-study/kratos-blog/internal/conf"
+	"go-study/kratos-blog/internal/service"
+)
+
+// NewHTTPServer new a HTTP server.
+func NewHTTPServer(c *conf.Server, logger log.Logger, blog *service.BlogService) *http.Server {
+	opts := []http.ServerOption{
+		http.Middleware(
+			recovery.Recovery(),
+			tracing.Server(),
+			logging.Server(logger),
+			validate.Validator(),
+		),
+	}
+	if c.Http.Network != "" {
+		opts = append(opts, http.Network(c.Http.Network))
+	}
+	if c.Http.Addr != "" {
+		opts = append(opts, http.Address(c.Http.Addr))
+	}
+	if c.Http.Timeout != nil {
+		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
+	}
+	srv := http.NewServer(opts...)
+	v1.RegisterBlogServiceHTTPServer(srv, blog)
+
+	// add swagger
+	openAPIhandler := openapiv2.NewHandler()
+	srv.HandlePrefix("/q/", openAPIhandler)
+	return srv
+}
