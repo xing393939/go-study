e03b6addc0e3d8293889a2f00e4e36f7a7cf01f9
xing393939 <xing393939@users.noreply.github.com>
2022-05-10 20:20:24 +0800

    go study

---
+++ go-demo/memstats/mmap_share.c
@@ -0,0 +1,22 @@
+// 参考：https://phpor.net/blog/post/893
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/mman.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+int main(int ac,char**av) {
+	char*fp=(char*)mmap(NULL,1024*1024*200,PROT_READ,MAP_SHARED|MAP_ANONYMOUS,-1,0);
+	char c;
+	int i=0;
+	if(fp==MAP_FAILED) {
+		printf("error\n");
+		exit(1);
+	}
+	while(i++<1024*1024*100) {
+		c=*(fp+i);
+	}
+	sleep(20000000);
+	return 0;
+}
