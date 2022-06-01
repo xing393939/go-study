30406881a3df9a09d0aa58821a00546e1ff0e699
xing393939 <xing393939@users.noreply.github.com>
2022-06-01 17:28:44 +0800

    k

--- go-demo/net/main.go
+++ go-demo/net/main.go
@@ -4,7 +4,7 @@ import (
 	"net"
 )
 
-func testRedis() {
+func clientRedis() {
 	conn, _ := net.Dial("tcp", "127.0.0.1:6379")
 	_, _ = conn.Write([]byte("ping\r\n"))
 	buffer := make([]byte, 16)
@@ -12,10 +12,25 @@ func testRedis() {
 	println(string(buffer), count)
 }
 
-func main() {
+func clientICMP() {
 	ipAddr, _ := net.ResolveIPAddr("ip4", "127.0.0.1")
 	conn, _ := net.ListenIP("ip4:icmp", ipAddr)
 	buf := make([]byte, 1024)
 	n, addr, _ := conn.ReadFrom(buf)
 	println(n, addr)
+}
+
+func serverTCP() {
+	tcpAddr, _ := net.ResolveTCPAddr("", "127.0.0.1:8080")
+	listener, _ := net.ListenTCP("tcp4", tcpAddr)
+	defer listener.Close()
+	for {
+		conn, _ := listener.AcceptTCP()
+		go func() {
+			buffer := make([]byte, 16)
+			_, _ = conn.Read(buffer)
+			_, _ = conn.Write(append([]byte{':'}, buffer...))
+			_ = conn.Close()
+		}()
+	}
 }
